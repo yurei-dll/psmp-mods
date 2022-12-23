3007f1a48c0342ca770c695240ce0e6c57ea039f
Yūrei.dll <69333284+yurei-dll@users.noreply.github.com>
2022-12-22 22:22:05 -0600

    Create listGen.js

---
+++ listGen.js
@@ -0,0 +1,8 @@
+// yurei_dll
+// A simple node.js script to help automate building mod lists using Curseforge
+// and a perfect example of my fucked up priorities
+//
+
+import puppeteer from 'puppeteer';
+
+
